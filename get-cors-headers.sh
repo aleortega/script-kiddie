178f49fa40a28800908bd9937c61da899c25c3d3
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2022-12-30 10:49:21 -0300

    add script to get CORS headers

---
+++ get-cors-headers.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+TARGET_URL="https://google.com"   # for sample only
+ORIGIN_HOST="https://example.com" # for sample only
+
+echo "Calling:    ${TARGET_URL}"
+echo "From:       ${ORIGIN_HOST}"
+echo "----"
+
+curl -I ${TARGET_URL} -X OPTIONS \
+  -H "Access-Control-Request-Method: OPTIONS" \
+  -H "Origin: ${ORIGIN_HOST}"
+# CHECK for the "access-control-allow-" headers in the response
